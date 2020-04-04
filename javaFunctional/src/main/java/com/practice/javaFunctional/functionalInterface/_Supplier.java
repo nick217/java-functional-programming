4cf442140214bde6f6f24b5ec0b61c113454def5
Nikhil Raheja <nick170294@gmail.com>
2020-04-05 00:19:43 +0530

    exploring functional interfaces

---
+++ javaFunctional/src/main/java/com/practice/javaFunctional/functionalInterface/_Supplier.java
@@ -0,0 +1,23 @@
+package com.practice.javaFunctional.functionalInterface;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Supplier;
+
+public class _Supplier {
+
+    public static void main(String[] args) {
+        System.out.println(getDBConnectionUrl());
+        System.out.println(getDBConnectionUrlsSupplier.get());
+    }
+
+    static String getDBConnectionUrl() {
+        return "jdbc://localhost:5432/users";
+    }
+
+    static Supplier<List<String>> getDBConnectionUrlsSupplier = ()
+            -> Arrays.asList(
+            "jdbc://localhost:5432/users",
+            "jdbc://localhost:5432/customer");
+
+}
