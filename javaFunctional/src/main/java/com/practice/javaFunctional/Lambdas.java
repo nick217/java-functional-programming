6a25941d45af4ace6e0be7f5219625d878cdacb2
Nikhil Raheja <nick170294@gmail.com>
2020-04-06 00:50:03 +0530

    Streams, Lambdas and Callbacks

---
+++ javaFunctional/src/main/java/com/practice/javaFunctional/Lambdas.java
@@ -0,0 +1,23 @@
+package com.practice.javaFunctional;
+import java.util.function.BiFunction;
+
+public class Lambdas {
+
+    public static void main(String[] args) {
+
+        Integer number = null;
+        int couter = 0;
+
+        BiFunction<String, Integer, String> upperCaseName = (name, age) -> {
+            int counter = 0;
+            // logic
+            if (name.isEmpty()) throw new IllegalArgumentException("");
+            System.out.println(age);
+            return name.toUpperCase();
+        };
+
+        System.out.println(upperCaseName.apply("Alex", null));
+
+    }
+
+}
